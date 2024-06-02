d9e3a8461b3d3ea811aedf1932fba2606e628b2a
amey0307 <ameytrips0307@gmail.com>
2024-06-02 20:39:55 +0530

    chore: Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO
+)
+.then(()=>{
+    console.log("Mongoose is connected");
+})
+.catch((e)=>{
+    console.log(e);
+})
 
 const app = express();
 
 const port = 3000;
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
